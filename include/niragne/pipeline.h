81e24dc6808481b579463b17b5e2f6f9bd432445
Nicolas IRAGNE <nicolas.iragne@alyce.fr>
2022-09-26 01:20:19 +0200

    kinda working

--- include/niragne/pipeline.h
+++ include/niragne/pipeline.h
@@ -17,61 +17,6 @@
 #include <string>
 #include <vector>
 
-/**
- # Pipelines dpy
-
-
-
-Uniformiser les types de processus, en utilisant la meme structure de pipeline:
-
-
-
-On considère 3 types de processus:
-
-- player: juste relire la video et eventuellement les données
-
-- process: lire la video et appliquer le deep learning dessus
-
-- demo: les deux en meme temps, plus ou moins
-
-
-
-Si on considere nos nodes de pipeline comme Node<Input, Output>, on peut eventuellement avoir quelque chose comme:
-
-
-
-Dans le cas du simple player, on va entrer en input un temps T et on veut en sortie une Frame F qui contient nos objets qui sont chargés via des fichiers deja presents
-
-
-
-On peut imaginer les nodes suivants:
-
-- Generator: Node<T, F>
-
-- Loader: Node<F, F>
-
-
-
-Maintenant on veut faire de la prod. On a, pareil, un temps T en input, mais en sortie on veut une PFrame pour mettre dans le buffer
-
-on peut imaginer une pipeline un peu comme
-
-- Generator: Node<T, F>
-
-- Detector: Node<F, PF>
-
-- Analyzer: Node<PF, PF>
-
-- Exporter: Node<PF, PF>
-
-
-
-En fait ce que je veux dire c'est qu'on pourrait faire des process compatibles meme si ils veulent un output different, parce que les nodes sont capables de changer le type de donnee
-
-EN gardant a l'esprit que l'objet est move entre chaque node, donc pas de copie
-
- */
-
 namespace niragne
 {
 
@@ -90,25 +35,19 @@ public:
     using OutputType = Output;
 
     Node() = default;
+
     Node(const ProcessFunction& process)
         : m_function(process)
     {
     }
+
+    ~Node() = default;
 
     virtual Output process(Input input)
     {
         return m_function(input);
     }
 };
-
-template <typename N1, typename N2>
-Node<typename N1::InputType, typename N2::OutputType> operator>>(const N1& n1, const N2& n2)
-{
-    return Node<typename N1::InputType, typename N2::OutputType>([&n1, &n2](auto input) {
-        return n2.process(n1.process(input));
-    });
-}
-
 
 class Incrementer : public Node<int, int>
 {
@@ -142,17 +81,60 @@ public:
     }
 };
 
-class Printer : public Node<std::string, void>
+class Printer : public Node<std::string, std::string>
 {
 public:
     Printer() = default;
 
     virtual ~Printer() = default;
 
-    void process(std::string input) override
+    std::string process(std::string input) override
     {
         std::cout << input << std::endl;
+        return input;
     }
 };
+
+template <typename T>
+class PassThrough : public Node<T, T>
+{
+public:
+    PassThrough() = default;
+
+    virtual ~PassThrough() = default;
+
+    T process(T input) override
+    {
+        return input;
+    }
+};
+
+template <typename N1, typename N2>
+Node<typename N1::InputType, typename N2::OutputType> operator>>(N1& n1, N2& n2)
+{
+    return Node<typename N1::InputType, typename N2::OutputType>([n1, n2](auto input) mutable
+        { return n2.process(n1.process(input)); });
+}
+
+template <typename N1, typename N2>
+Node<typename N1::InputType, typename N2::OutputType> operator>>(N1&& n1, N2& n2)
+{
+    return Node<typename N1::InputType, typename N2::OutputType>([n1, n2](auto input) mutable
+        { return n2.process(n1.process(input)); });
+}
+
+template <typename N1, typename N2>
+Node<typename N1::InputType, typename N2::OutputType> operator>>(N1& n1, N2&& n2)
+{
+    return Node<typename N1::InputType, typename N2::OutputType>([n1, n2](auto input) mutable
+        { return n2.process(n1.process(input)); });
+}
+
+template <typename N1, typename N2>
+Node<typename N1::InputType, typename N2::OutputType> operator>>(N1&& n1, N2&& n2)
+{
+    return Node<typename N1::InputType, typename N2::OutputType>([n1, n2](auto input) mutable
+        { return n2.process(n1.process(input)); });
+}
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,43 @@
  */
 
 #include "pipeline.h"
+#include <filesystem>
 #include <iostream>
+#include <fstream>
+#include <type_traits>
+
 
 using namespace niragne;
 
 int main(int argc, char** argv)
 {
-    int i = 0;
-    auto a = Incrementer(2);
-    auto b = Incrementer(3);
-    auto c = Stringifier();
-    auto d = Printer();
+    auto fileloader = Node<std::filesystem::path, std::string>([](std::filesystem::path path) {
+        std::ifstream file(path);
+        std::cout << "Loading file " << path << std::endl;
+        std::string str((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        return str;
+    });
+    auto inverter = Node<std::string, std::string>([](std::string str) {
+        std::string inverted;
+        for (auto it = str.rbegin(); it != str.rend(); ++it)
+        {
+            inverted.push_back(*it);
+        }
+        return inverted;
+    });
+    auto filewriter = Node<std::string, std::filesystem::path>([](std::string str) {
+        std::filesystem::path path = "output.txt";
+        std::ofstream file(path);
+        std::cout << "Writing file " << path << std::endl;
+        file << str;
+        return path;
+    });
 
-    auto e = a >> b;
-    return 0;
+    auto printer = Node<std::string, std::string>([](std::string str) {
+        std::cout << str << std::endl;
+        return str;
+    });
+
+    auto pipeline = fileloader >> printer >> inverter >> printer >> filewriter;
+    pipeline.process("input.txt");
 }
